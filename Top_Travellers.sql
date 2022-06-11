260f91ff282363986b847364c96f16a93e4fc3d7
맹북 <mjy9412@naver.com>
2022-06-11 22:04:15 +0900

    leetcode_join

---
+++ Top_Travellers.sql
@@ -0,0 +1,7 @@
+-- 여행자들의 거리를 조회
+SELECT name
+     , IFNULL(SUM(distance), 0) AS travelled_distance
+FROM Users u
+    LEFT JOIN Rides r ON u.id = r.user_id
+GROUP BY u.id
+ORDER BY travelled_distance DESC, name
